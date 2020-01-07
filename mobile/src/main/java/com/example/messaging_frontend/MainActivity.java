d6d18b29b75872c65bf435ce30a371f16112e6d1
Furkan <tas.furkan@gmx.de>
2020-01-07 14:09:11 +0100

    Added listener to UI

--- mobile/src/main/java/com/example/messaging_frontend/MainActivity.java
+++ mobile/src/main/java/com/example/messaging_frontend/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.messaging_frontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
