10eeb0c9dafe505378a9329928158eab48c2ba56
Feriel Amira <ferielamira@feriels-mbp.speedport.ip>
2020-01-21 23:35:52 +0100

    - Communication between ConvActivity and ConvList

--- mobile/src/main/java/com/example/messaging_frontend/ConversationActivity.java
+++ mobile/src/main/java/com/example/messaging_frontend/ConversationActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,9 +36,11 @@ public class ConversationActivity extends AppCompatActivity {
     private ConversationAdapter mMessageAdapter;
     List<Message> messageList;
     Button sendButton;
+    Button returnButton;
     EditText messageSent;
     Contact mSender;
     Date mDate;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,10 +59,17 @@ public class ConversationActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
                 send_message();
+            }
+        });
+        returnButton =  (Button) findViewById(R.id.return_button);
 
-
+        returnButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                Intent intent =  new Intent(context, ConversationsListActivity.class);
+                ((Context) context).startActivity(intent);
             }
         });
 
@@ -66,8 +77,28 @@ public class ConversationActivity extends AppCompatActivity {
 
     private List<Message> mockMessageList() {
          Bundle convID=  getIntent().getExtras();
+         if(convID.containsKey("ConversationID")) {
+            //this will be dynamic once we have the actual messages
+             if (convID.get("ConversationID").equals(1)) {
+                 List<Message> messageList = new ArrayList<>();
 
-         if(convID.get("ConversationID").equals(1)) {
+                 Date date1 = new Date();
+                 Contact sender = new Contact("Mahmoud", 1);
+                 Message message1 = new Message(sender, "Hey Feriel, how are you?", date1.getTime());
+
+                 Date date2 = new Date();
+                 Contact receiver = new Contact("Feriel", 2);
+                 Message message2 = new Message(receiver, "I'm tired but I will survive", date2.getTime());
+
+                 messageList.add(message1);
+                 messageList.add(message2);
+
+                 return messageList;
+             }
+         }
+
+         //I created this to test directly
+         else{
              List<Message> messageList = new ArrayList<>();
 
              Date date1 = new Date();
@@ -83,6 +114,9 @@ public class ConversationActivity extends AppCompatActivity {
 
              return messageList;
          }
+
+
+
          return null;
     }
 
